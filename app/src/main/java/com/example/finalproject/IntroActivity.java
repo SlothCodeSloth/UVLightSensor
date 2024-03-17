4fe8b6bf8a957b331dee0a9d7b333ed42eedfa54
Yaaaaay <93399548+Yaaaaay@users.noreply.github.com>
2024-03-16 22:10:39 -0400

    Version 0.1 - Bottom Navigation implemented. Fragments are correctly shown.

---
+++ app/src/main/java/com/example/finalproject/IntroActivity.java
@@ -0,0 +1,147 @@
+package com.example.finalproject;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntroActivity extends AppCompatActivity {
+
+    private ViewPager2 screenPager;
+    IntroViewPagerAdapter introViewPagerAdapter;
+    TabLayout tabIndicator;
+    Button btnNxt;
+    int position = 0;
+    Button btnGetStarted;
+    Animation btnAnim;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        if (restorePrefData()) {
+            Intent mainActivity = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(mainActivity);
+            finish();
+        }
+
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_intro);
+
+        WindowInsetsController insetsController = getWindow().getInsetsController();
+        if (insetsController != null) {
+            insetsController.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+        }
+
+        //getSupportActionBar().hide();
+
+        btnNxt = findViewById(R.id.btn_next);
+        btnGetStarted = findViewById(R.id.btn_get_started);
+        tabIndicator = findViewById(R.id.tab_indicator);
+        btnAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.button_animation);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        List<ScreenItem> mList = new ArrayList<>();
+        mList.add(new ScreenItem("Fresh Food", "Vewy Fast!", R.drawable.img1));
+        mList.add(new ScreenItem("Fast Delivery", "Vewy Deliver!", R.drawable.img2));
+        mList.add(new ScreenItem("Easy Payment", "Vewy Money!", R.drawable.img3));
+
+        screenPager = findViewById(R.id.screen_viewpager);
+        introViewPagerAdapter = new IntroViewPagerAdapter(this, mList);
+        screenPager.setAdapter(introViewPagerAdapter);
+
+        new TabLayoutMediator(tabIndicator, screenPager,
+                (tab, position) -> {}
+        ).attach();
+
+        btnNxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                position = screenPager.getCurrentItem();
+                if (position < mList.size()) {
+                    position++;
+                    screenPager.setCurrentItem(position);
+                }
+
+                if (position == mList.size() - 1) {
+                    loadLastScreen();
+                }
+            }
+        });
+
+        tabIndicator.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getPosition() == mList.size() - 1) {
+                    loadLastScreen();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        btnGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mainActivity = new Intent (getApplicationContext(), MainActivity.class);
+                startActivity(mainActivity);
+                savePrefsData();
+                finish();
+            }
+        });
+    }
+
+    private boolean restorePrefData() {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("myPrefs", MODE_PRIVATE);
+        boolean isIntroActivityOpenedBefore = pref.getBoolean("isIntroOpened", false);
+        return isIntroActivityOpenedBefore;
+    }
+
+    private void savePrefsData() {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("myPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putBoolean("isIntroOpened", true);
+        editor.apply();
+    }
+
+    private void loadLastScreen() {
+        btnNxt.setVisibility(View.INVISIBLE);
+        btnGetStarted.setVisibility(View.VISIBLE);
+        tabIndicator.setVisibility(View.INVISIBLE);
+        btnGetStarted.setAnimation(btnAnim);
+    }
+}
